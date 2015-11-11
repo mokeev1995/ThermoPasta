6ab3b36692d57c3bf511a44cd33959788833d035
Vinogradov Kirill <vikian050194@gmail.com>
2015-11-11 14:59:32 +0300

    add profilecontroller

---
+++ Portal/Portal/Controllers/ProfileController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Ninject.Infrastructure.Language;
+using Portal.DAL;
+using Portal.Models.ViewModels;
+
+namespace Portal.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IUnitOfWork _uow;
+        public ProfileController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        // GET: Profile
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var profiles = _uow.ProfileRepository.GetAll().Where(p => p.UserDataId == null || p.UserDataId == userId).ToEnumerable();
+            var profilesList = new List<ProfileView>();
+
+            foreach (var profile in profiles)
+            {
+                profilesList.Add(new ProfileView
+                {
+                    Id = profile.Id,
+                    Title = profile.Title,
+                    Description = profile.Description,
+                    IsDefault = profile.UserDataId == null
+                });
+            }
+
+            return View(profilesList);
+        }
+
+        // GET: Profile/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Profile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profile/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Portal/Portal/Models/ViewModels/Thermo.cs
+++ Portal/Portal/Models/ViewModels/Thermo.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Portal.Models.ViewModels
 {
@@ -18,6 +19,59 @@ namespace Portal.Models.ViewModels
     }
 
     public class DeviceCreate
+    {
+        [Required]
+        [StringLength(256)]
+        [MinLength(2)]
+        public string Title { get; set; }
+
+        public int ProfileId { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        [MinLength(4)]
+        public string Code { get; set; }
+    }
+
+        public class ProfileView
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsDefault { get; set; }
+    }
+
+    public class ProfileCreate
+    {
+        [Required]
+        [StringLength(256)]
+        [MinLength(2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [MinLength(2)]
+        public string Description { get; set; }
+    }
+
+    public class IntervalView
+    {
+        public int Id { get; set; }
+
+        public int Start { get; set; }
+
+        public int End { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [MinLength(2)]
+        public string Description { get; set; }
+    }
+
+    public class IntervalCreate
     {
         [Required]
         [StringLength(256)]
