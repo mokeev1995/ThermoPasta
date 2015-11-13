7ef9b4fb8b30dccaa68775f4a49baf95e8b7a29e
Vinogradov Kirill <vikian050194@gmail.com>
2015-11-13 20:08:25 +0300

    fix bug with unique checkcode

--- Portal/Portal/Controllers/InteractionController.cs
+++ Portal/Portal/Controllers/InteractionController.cs
@@ -21,23 +21,28 @@ namespace Portal.Controllers
         {
             if (id != null)
             {
-                var random = new Random();
-                var newCode = "";
-                do
+                if (!_uow.CheckCodeRepository.GetAll().Any(cc => cc.Id == id))
                 {
-                    newCode = string.Format("{0:0000}", random.Next(0, 10000));
-                } while (_uow.CheckCodeRepository.GetAll().Any(cc => cc.Code == newCode));
+                    var random = new Random();
+                    var newCode = "";
+                    do
+                    {
+                        newCode = string.Format("{0:0000}", random.Next(0, 10000));
+                    } while (_uow.CheckCodeRepository.GetAll().Any(cc => cc.Code == newCode));
 
-                var newCheckCode = new CheckCode
-                {
-                    Code = newCode,
-                    Id = id,
-                    Time = DateTime.Now
-                };
-                _uow.CheckCodeRepository.Insert(newCheckCode);
-                _uow.Save();
+                    var newCheckCode = new CheckCode
+                    {
+                        Code = newCode,
+                        Id = id,
+                        Time = DateTime.Now
+                    };
+                    _uow.CheckCodeRepository.Insert(newCheckCode);
+                    _uow.Save();
 
-                return newCode;
+                    return newCode;
+                }
+
+                return "Already added.";
             }
 
             return "Error.";
@@ -60,8 +65,10 @@ namespace Portal.Controllers
                 _uow.TemperatureRepository.Insert(newTemperature);
                 _uow.Save();
 
-                var period = devices[0].Period;
-              
+                //var period = devices[0].Period;
+                var r = new Random();
+                var period = r.Next(1,3);
+
                 Debug.Print(period.ToString());
                 return period;
             }
