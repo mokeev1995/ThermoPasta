789f147e257f7945b6385843860d3b5019981878
mokeev1995 <andrey@mokeev1995.ru>
2015-11-14 17:45:54 +0300

    some design updates

--- Portal/Portal/App_Start/RouteConfig.cs
+++ Portal/Portal/App_Start/RouteConfig.cs
@@ -8,7 +8,7 @@ namespace Portal
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+	        routes.LowercaseUrls = true;
             routes.MapRoute("Default", "{controller}/{action}/{id}",
                 new {controller = "Home", action = "Index", id = UrlParameter.Optional}
                 );
